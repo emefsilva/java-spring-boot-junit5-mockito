7eea528138d027edb25b1e99aa394392d1c58261
Emerson <emersonfdasilva2@gmail.com>
2023-10-15 22:02:35 -0300

    Criação do projeto java - estrutura

---
+++ src/test/java/io/github/emefsilva/api/ApiSpringJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emefsilva.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
