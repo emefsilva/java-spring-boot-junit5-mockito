1e9daaf65b3e4cc1e88a4f99db6a8f5759d53e24
Emerson <emersonfdasilva2@gmail.com>
2023-10-18 22:52:13 -0300

    correção DTO  fixed #9

--- src/main/java/io/github/emefsilva/api/domain/dto/UserDTO.java
+++ src/main/java/io/github/emefsilva/api/domain/dto/UserDTO.java
@@ -3,14 +3,12 @@ package io.github.emefsilva.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
-
 @Data
 public class UserDTO {
 
     private Integer id;
     private String name;
     private String email;
-
     @JsonIgnore
     private String password;
 }
