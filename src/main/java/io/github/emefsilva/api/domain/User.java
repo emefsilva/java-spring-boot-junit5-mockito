6dcbec3081649bdd2ffd58b9951a04b9546601db
Emerson <emersonfdasilva2@gmail.com>
2023-10-29 18:31:33 -0300

    corrigindo imports do projeto

--- src/main/java/io/github/emefsilva/api/domain/User.java
+++ src/main/java/io/github/emefsilva/api/domain/User.java
@@ -1,20 +1,20 @@
 package io.github.emefsilva.api.domain;
 
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity(name = "TB_USER")
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include
     private Integer id;
     private String name;
 
